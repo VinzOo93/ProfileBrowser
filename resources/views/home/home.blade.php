f64754be86248edbbd49e5b5c9324191ad3c0562
VinzOo93 <v.orru@live.fr>
2021-05-16 15:28:27 +0200

    responsive and css home ok

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,14 +5,15 @@
 
 <div class="tab-profile">
     @foreach($profiles as $profile)
-    <div class="col">
-              <button  id="{{$profile->id}}" class="button-raw">{{$profile->name}}  </button>
-        <div class="content">
-            <h3>{{$profile->name}}</h3>
-            <img class="profile-img" src="{{asset('images/'.$profile->photo)}}" alt="img-{{$profile->name}}">
-            <p>{{$profile->description}}</p>
+        <div class="col">
+            <button id="{{$profile->id}}" class="button-raw">{{$profile->name}}  </button>
+            <div id="content-{{$profile->id}}" class="content">
+                <div class="conainer">
+
+                    <h3>{{$profile->name}}</h3>
+                    <p><img class="profile-img" src="{{asset('images/'.$profile->photo)}}" alt="img-{{$profile->name}}">{{$profile->description}}</p></div>
+            </div>
         </div>
-    </div>
 
     @endforeach
 </div>
