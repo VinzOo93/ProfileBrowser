2c7a96be267c73ad0fedb789b274941591576667
VinzOo93 <v.orru@live.fr>
2021-05-17 12:40:22 +0200

    index crud finished

--- resources/views/profiles/profile_index.blade.php
+++ resources/views/profiles/profile_index.blade.php
@@ -6,13 +6,45 @@
         <nav>
             <ul>
                 <li>
-                    <a href="{{url('/')}}">home</a>
+                    <a href="{{url('/')}}">Home</a>
+                </li>
+                <li>
+                    <a class="create" href="">New Profile</a>
                 </li>
             </ul>
         </nav>
     </div>
-    @foreach($profiles as $profile)
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Photo name</th>
+            <th>Date created</th>
+            <th>Last update</th>
+            <th>action</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($profiles as $profile)
+            <tr>
+                <td class="id">{{ $profile->id }}</td>
+                <td class="name">{{ $profile->name }}</td>
+                <td class="description">{{ $profile->description }}</td>
+                <td class="photo">{{ $profile->photo }}</td>
+                <td class="created_at">{{ $profile->created_at }}</td>
+                <td class="updated_at">{{ $profile->updated_at }}</td>
+                <td>
+                    <a href="">show</a>
+                    <a href="">update</a>
+                    <a href="">delete</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
 
-    @endforeach
+    </table>
+
 </div>
 
